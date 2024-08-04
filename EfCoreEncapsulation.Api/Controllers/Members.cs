d9cfdb0c77a79c860db2626b1fe6e70b1fe9bd18
jmayorga94 <javiermay@iadb.org>
2024-08-04 19:28:30 -0400

    Add: Members endpoint

---
+++ EfCoreEncapsulation.Api/Controllers/Members.cs
@@ -0,0 +1,40 @@
+﻿using EfCoreEncapsulation.Api.DTOs.Members.Responses;
+using EfCoreEncapsulation.Api.Infrastructure.Persistence;
+using EfCoreEncapsulation.Api.Members;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EfCoreEncapsulation.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Members : ControllerBase
+    {
+        private readonly GymContext _context;
+        public Members(GymContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("id")]
+        public IActionResult Get(int id)
+        {
+            var result =  _context.Members.Find(id);
+
+            if(result is null)
+            {
+                return NotFound();
+            }
+
+            var response = new MembersResponse()
+            {
+                Id = result.MemberId,
+                MemberSince = result.MembershipStartDate,
+                Name = result.Name,
+            };
+
+            return Ok(response);
+
+        }
+    }
+}
---
+++ EfCoreEncapsulation.Api/DTOs/Members/Responses/MembersResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace EfCoreEncapsulation.Api.DTOs.Members.Responses
+{
+    public record MembersResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime MemberSince { get; set; }
+    }
+}
